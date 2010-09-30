cbe4f138c51238147e43e014d0999dbf23627b85
Jay McCarthy <jay.mccarthy@gmail.com>
2010-09-30 16:47:15 -0600

    info

--- info.rkt
+++ info.rkt
@@ -5,9 +5,11 @@
 (define scribblings '(["scribblings/opencl.scrbl" (multi-page)]))
 (define categories '(devtools))
 (define primary-file "racket.rkt")
-(define compile-omit-paths '("examples" "tests"))
+(define compile-omit-paths '("examples" "tests" "samples"))
 (define version "1.0.48")
 (define release-notes 
   (list
-   '(ul (li "Windows support added"))))
+   '(ul (li "Racketizing")
+        (li "Better contracts")
+        (li "Linux & ATI support"))))
 (define repositories '("4.x"))
